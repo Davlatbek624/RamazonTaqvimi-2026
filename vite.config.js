a5239ae8f862630ab5f0c541c73a9f33a3f7917f
Davlatbek624 <ashurovf018@gmail.com>
2026-02-18 19:50:03 +0500

    Play

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { VitePWA } from 'vite-plugin-pwa'
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto', // Avtomatik service worker ro'yxatga olish
+      manifest: {
+        name: 'Ramazon Taqvimi 2026',
+        short_name: 'Ramazon2026',
+        description: '2026-yilgi Ramazon taqvimi. Saharlik va iftorlik vaqtlari.', // Tavsif qo'shildi
+        theme_color: '#000000',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/logo.png', // Slash bilan yozing
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any' 
+          },
+          {
+            src: '/logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable' // Play Market uchun muhim
+          }
+        ]
+      }
+    })
+  ]
 })
